2b3ed7d62b5ce85814ae3b22d5f0f8a40843d160
Vlada Vasileva <vvavlada@gmail.com>
2026-02-12 22:58:46 +0300

    KTB-07 - создала цикл изучения слов

--- src/main/kotlin/additional/File.kt
+++ src/main/kotlin/additional/File.kt
@@ -1,4 +1,5 @@
 package org.example.additional
+const val CORRECTANSWERS = 3
 
 fun main() {
     val dictionary = loadDictionary()
@@ -15,14 +16,35 @@ fun main() {
         val input = readln()
 
         when (input) {
-            "1" -> println("Учить слова.")
+            "1" -> {
+                println("Учить слова:")
+                while (true) {
+                    val notLearnedList = dictionary.filter { it.correctAnswersCount < 3 }
+                    if (notLearnedList.isEmpty()) {
+                        println("Все слова в словаре выучены")
+                        break
+                    } else {
+                        val questionWords: List<Word> = notLearnedList.take(4).shuffled()
+                        val correctAnswer = questionWords.random()
+                        val options = questionWords.map { it.translation }.shuffled()
+                        println()
+                        println("${correctAnswer.word}:")
+                        options.forEachIndexed { index, transletion ->
+                            println("${index + 1} - $transletion")
+                        }
+                        println("Введите номер правильного ответа: ")
+                        val userInput = readln()
+                        println("Ваш ответ: $userInput")
+                    }
+                }
+            }
             "2" -> {
                 println("Статистика:")
                 val totalCount = dictionary.size
                 println("Всего слов в словаре: $totalCount")
-                val learnedWords = dictionary.filter { it.correctAnswersCount >= 3 }
+                val learnedWords = dictionary.filter { it.correctAnswersCount >= CORRECTANSWERS }
                 val learnedCount = learnedWords.size
-                val percent = if (learnedCount > 0) {
+                val percent = if (totalCount > 0) {
                     (learnedCount * 100) / totalCount
                 } else 0
                 println("Выучено $learnedCount из $totalCount слов | $percent%")
