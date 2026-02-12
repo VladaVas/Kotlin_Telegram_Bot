c5bbd05eaea118dd34413cd77f196a8d4374f5a0
Vlada Vasileva <vvavlada@gmail.com>
2026-02-12 19:44:31 +0300

    KTB-05 - исправила замечания + выполнила шаги по заданию

--- src/main/kotlin/additional/File.kt
+++ src/main/kotlin/additional/File.kt
@@ -1,27 +1,31 @@
 package org.example.additional
 
 import java.io.File
-// import java.util.Dictionary
 
 fun main() {
+    val dictionary = loadDictionary()
+    println("Загружено слов: ${dictionary.size}")
 
-    val wordsFile: File = File("word.txt")
-    val dictionary: MutableList<Word> = mutableListOf()
-    val lines: List<String> = wordsFile.readLines()
+    while (true) {
+        println()
+        println("Меню: ")
+        println("1 - Учить слова")
+        println("2 - Статистика")
+        println("0 - Выход")
+        println("Ваш выбор: ")
 
-    for (line in lines) {
-        val line = line.split("|")
-        val word = Word(word = line[0], translation = line[1], correctAnswersCount = line.getOrNull(2)?.toIntOrNull() ?: 0)
-        dictionary.add(word)
+        val input = readln();
+
+        when (input) {
+            "1" -> println("Учить слова")
+            "2" -> println("Статистика")
+            "0" -> {
+                println("Выход из программы")
+                break
+            }
+
+            else -> println("'Введите число 1, 2 или 0")
+        }
+
     }
-    println(dictionary)
-
-    // wordsFile.createNewFile()
-    // wordsFile.writeText("hello - привет\n")
-  //  wordsFile.appendText("dog - собака\n")
-   // wordsFile.appendText("cat - кошка\n")
-
-   // for (word in wordsFile.readLines()) {
-     //   println(word)
-    //}
 }
--- src/main/kotlin/additional/Word.kt
+++ src/main/kotlin/additional/Word.kt
@@ -1,8 +1,26 @@
 package org.example.additional
 
+import java.io.File
+
 data class Word(
     val word: String,
     val translation: String,
-    val correctAnswersCount: Int? = 0,
-) {
+    val correctAnswersCount: Int = 0,
+)
+
+fun loadDictionary(): List<Word> {
+    val wordsFile: File = File("word.txt")
+    val dictionary: MutableList<Word> = mutableListOf()
+    val lines: List<String> = wordsFile.readLines()
+
+    for (line in lines) {
+        val parts = line.split("|")
+        val word = Word(
+            word = parts[0],
+            translation = parts[1],
+            correctAnswersCount = parts.getOrNull(2)?.toIntOrNull() ?: 0
+        )
+        dictionary.add(word)
+    }
+    return dictionary
 }
